61134967f006d84dee42aee9d67eafb03028ea6f
KDS4wexp@gmail.com <KDS4wexp@gmail.com>
2024-05-13 15:16:50 +0300

    add PRIVATE_HOST, PUBLIC_HOST, inventory.yml, playbook.yml | modify dns_recordset.tf, dns_zone.tf

--- example_1/terraform/dns_recordset.tf
+++ example_1/terraform/dns_recordset.tf
@@ -7,16 +7,20 @@ resource "yandex_dns_recordset" "bastion-recordset" {
 }
 
 resource "yandex_dns_recordset" "other-vm-recordset" {
-    zone_id = yandex_dns_zone.public-zone.id
-    name = "other-vm"
+    for_each = tomap({
+        vm-0 = yandex_compute_instance.compute-instance[0].network_interface.0.ip_address
+        vm-1 = yandex_compute_instance.compute-instance[1].network_interface.0.ip_address
+        vm-2 = yandex_compute_instance.compute-instance[2].network_interface.0.ip_address
+        vm-3 = yandex_compute_instance.compute-instance[3].network_interface.0.ip_address
+        vm-4 = yandex_compute_instance.compute-instance[4].network_interface.0.ip_address
+        vm-5 = yandex_compute_instance.compute-instance[5].network_interface.0.ip_address
+        })
+    zone_id = yandex_dns_zone.private-zone.id
+    name = "${each.key}"
     type = "A"
     ttl = 200
-    data = [yandex_compute_instance.compute-instance[0].network_interface.0.ip_address,
-            yandex_compute_instance.compute-instance[1].network_interface.0.ip_address,
-            yandex_compute_instance.compute-instance[2].network_interface.0.ip_address,
-            yandex_compute_instance.compute-instance[3].network_interface.0.ip_address,
-            yandex_compute_instance.compute-instance[4].network_interface.0.ip_address,
-            yandex_compute_instance.compute-instance[5].network_interface.0.ip_address]
+    data = [each.value]
+    depends_on = [ yandex_compute_instance.compute-instance ]
 }
 
 resource "yandex_dns_recordset" "gitlab-recordset" {
--- example_1/terraform/dns_zone.tf
+++ example_1/terraform/dns_zone.tf
@@ -1,17 +1,14 @@
 resource "yandex_dns_zone" "public-zone" {
     name = "public-zone"
     folder_id = var.folder-id
-    zone = "public.com."
+    zone = "kds4wexp.ru."
     public = true
-    deletion_protection = true
 }
 
 resource "yandex_dns_zone" "private-zone" {
     name = "private-zone"
     folder_id = var.folder-id
-    zone = "private.com."
+    zone = "private.ru."
     public = false
     private_networks = [yandex_vpc_network.internal-bastion-network.id]
-
-    deletion_protection = true
 }
